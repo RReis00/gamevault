2fc29bc8dfa08ee2355b6e2e87c4989d8bff2835
Rodri <136265952+RReis00@users.noreply.github.com>
2025-07-24 19:22:42 +0100

    first development, some routes and store.js

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites() {
+    return (
+        <div className="text-center mt-5">
+            <h1>⭐ Favorites</h1>
+            <p>Your favorite games show up here.</p>
+        </div>
+    )
+}
+
+export default Favorites;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="text-center mt-5">
+      <h1>🏠 Home</h1>
+      <p>Welcome to GameVault</p>
+    </div>
+  );
+}
+
+export default Home;
