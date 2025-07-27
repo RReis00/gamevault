80dc5de773e6b7ef1d66d9259dc383223c0cd4b6
Rodri <136265952+RReis00@users.noreply.github.com>
2025-07-27 18:07:45 +0100

    favorites page showing favorite games

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,25 @@
+import { useSelector } from "react-redux";
+import GameCard from "../components/GameCard";
+
 function Favorites() {
-    return (
-        <div className="text-center mt-5">
-            <h1>⭐ Favorites</h1>
-            <p>Your favorite games show up here.</p>
+  const favorites = useSelector((state) => state.favorites.list);
+
+  return (
+    <div className="text-center mt-5">
+      <h1 className="text-center mb-4">Favorites</h1>
+      {favorites.length === 0 ? (
+        <p className="text-center">No Favorite games yet</p>
+      ) : (
+        <div className="row">
+          {favorites.map((game) => (
+            <div key={game.id} className="col-md-4 mb-4">
+              <GameCard game={game} />
+            </div>
+          ))}
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
 export default Favorites;
